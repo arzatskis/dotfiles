770ade1e25308863fbf14f4feb2d7f3fc2dc886a
arzatskis <hello@edvinas.lt>
2018-06-01 11:37:43 +0300

    Add Dock preferences

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -39,3 +39,8 @@ defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+# Remove Dock autohide delay
+defaults write com.apple.dock autohide-delay -float 0
+defaults write com.apple.dock autohide-time-modifier -float 0.5; 
+killall Dock
