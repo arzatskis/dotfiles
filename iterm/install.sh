278c3c75a30cecf117b6784338d526f6a23481e8
arzatskis <hello@edvinas.lt>
2020-05-02 12:19:10 +0300

    Move sublime setup script to proper location

--- iterm/install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-ln -s ~/.dotfiles/sublime/User ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
-
--- sublime/install.sh
+++ sublime/install.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ln -s
+ln -s ~/.dotfiles/sublime/User ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
