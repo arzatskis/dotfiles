a8a8a3341a0ccf7df47d8c7ae4d5d1e8ae70c7a6
Edvinas Aržatskis <hello@edvinas.lt>
2026-04-16 20:00:25 +0300

    Latest

--- node/install.sh
+++ node/install.sh
@@ -7,9 +7,9 @@ then
   export NVM_DIR="$HOME/.nvm"
   [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"
   [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"
-  nvm install 12
-  nvm use --delete-prefix v12
-  nvm alias default v12
+  nvm install 22
+  nvm use 22
+  nvm alias default 22
 else
   printf "\r \t Skipping \n"
 fi
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,6 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+alias oras='curl wttr.in/Vilnius'
 
-alias oras='curl wttr.in'
+alias yd='yarn && yarn dev'
